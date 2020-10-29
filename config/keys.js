916fb8f5f5ad64a590faeb3959af43c0a24a0cd1
akppan <akppan123@gmail.com>
2020-10-29 13:50:03 +0530

    creating models and input validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb+srv://root:root@cluster0.v1kwd.mongodb.net/devconnector1?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://root:root@cluster0.v1kwd.mongodb.net/devconnector1?retryWrites=true&w=majority',
+    secretKey:'secret'
 };
