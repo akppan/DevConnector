9bdd13e03daee0c599a21edc8482ca94e4b23843
akppan <akppan123@gmail.com>
2020-11-05 23:19:09 +0530

    profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER, GET_PROFILES } from './types';
 
 // Get current profile
 
@@ -14,6 +14,21 @@ export const getCurrentProfile = () => dispatch => {
     ).catch(err => dispatch({
         type:GET_PROFILE,
         payload:{}
+    }));
+}
+
+// Get profile by handle
+
+export const getProfileByHandle = (handle) => dispatch => {
+    dispatch(setProfileLoading());
+    axios.get(`/api/profile/handle/${handle}`)
+        .then(res => dispatch({
+            type:GET_PROFILE,
+            payload:res.data
+        })
+    ).catch(err => dispatch({
+        type:GET_PROFILE,
+        payload:null
     }));
 }
 
@@ -76,7 +91,22 @@ export const deleteEducation = (id) => dispatch => {
             payload:err.response.data
         })
     );
-}
+};
+
+//Get all profiles
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios.get('/api/profile/all')
+        .then(res => dispatch({
+            type:GET_PROFILES,
+            payload:res.data
+        }))
+        .catch(err => dispatch({
+            type:GET_PROFILES,
+            payload:null
+        })
+    );
+};
 
 
 
